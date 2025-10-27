a2c26bd518424263717d6809110373fcbc57c3d3
Marcones <141690889+Marconesds1@users.noreply.github.com>
2025-10-27 18:41:53 -0300

    Update loctracker.py

--- loctracker.py
+++ loctracker.py
@@ -35,7 +35,7 @@ def is_option(func):
 def IP_Track():
     ip = input(f"{Wh}\n DIGITE O IP: {Gr}")  # INPUT IP ADDRESS
     print()
-    print(f' {Wh}============= {Gr}SHOW INFORMATION IP ADDRESS {Wh}=============')
+    print(f' {Wh}============= {Gr}INFORMACOES DO IP {Wh}=============')
     req_api = requests.get(f"http://ipwho.is/{ip}")  # API IPWHOIS.IS
     ip_data = json.loads(req_api.text)
     time.sleep(2)
@@ -312,5 +312,6 @@ if __name__ == '__main__':
         time.sleep(2)
 
         exit()
+
 
 
